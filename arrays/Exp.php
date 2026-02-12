47aeb9949b2139180b19952ad2463604b4cce717
honore <honoreniyogushimwa63@gmail.com>
2026-02-12 19:15:34 +0200

    another changes

---
+++ arrays/Exp.php
@@ -0,0 +1,23 @@
+<?php
+/* In php there are 3 types of arrays:
+
+1. Indexed arrays: These are arrays that use numeric indexes to store and access values. The indexes start from 0 and increment by 1 for each element added to the array. Example:
+   $fruits = array("Apple", "Banana", "Cherry");
+
+
+⚠️ Note: We use [] tp create an indexed array and we can also use the array() function to create an indexed array.
+
+   
+2. Associative arrays: These are arrays that use named keys to store and access values. The keys can be strings or numbers, and they are used to identify the values in the array. Example:
+   $person = array("name" => "John", "age" => 30);
+   
+
+3. Multidimensional arrays: These are arrays that contain other arrays as their elements. They can be used to represent complex data structures, such as tables or matrices. Example:
+   $matrix = array(
+       array(1, 2, 3),
+       array(4, 5, 6),
+       array(7, 8, 9)
+   );
+   
+*/
+?>
