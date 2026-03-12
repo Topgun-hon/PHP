f7aec0241cc0c6e0155d3e26ae591e41ccb5586d
honore <honoreniyogushimwa63@gmail.com>
2026-03-12 16:35:02 +0200

    added files

---
+++ BACKEND CONNECTION/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = 'localhost';
+$username = 'honore';
+$password = 'topgun';
+$dbname = 'year1c';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if(!$conn){
+    exit('Connection Failed: '.mysqli_connect_error());
+}
+
+echo 'Connected Successfully';
+mysqli_close($conn);
+
+
+?>
