ad6137f1cc5f171c0b400d4d1d55db6b8b7dc61b
honore <honoreniyogushimwa63@gmail.com>
2026-05-17 16:21:14 +0200

    feat(): added session auth

---
+++ TODO/middleware/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    http_response_code(401);
+
+    echo json_encode([
+        'success' => false,
+        'message' => 'Unauthorized'
+    ]);
+
+    exit();
+}
+?>
