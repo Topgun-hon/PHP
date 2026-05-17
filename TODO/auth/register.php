dcc970f327527a6f858cf09ed91dbf361a16e08a
honore <honoreniyogushimwa63@gmail.com>
2026-05-17 15:47:40 +0200

    feat(): added pdo DB connection

--- TODO/auth/register.php
+++ TODO/auth/register.php
@@ -1,1 +1,22 @@
- 
+<?php
+
+$host = 'localhost';
+$dbname = 'todo';
+$username = 'honore';
+$password = 'topgun';
+
+try {
+    $pdo = new PDO("mysql:host=$host; dbname=$dbname; charset=utf8");
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+} catch (PDOException $e) {
+    die(json_encode([
+        'success' => false,
+        'message' => $e->getMessage()
+    ]));
+}
+
+?>
