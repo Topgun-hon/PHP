c0e606362ade1d470cd14c2bc1b1672fe963cc8a
honore <honoreniyogushimwa63@gmail.com>
2026-05-28 16:55:15 +0200

    Added protected property

---
+++ OOP/protected.php
@@ -0,0 +1,19 @@
+<?php
+
+class Secure
+{
+    protected $secrete = 'Codes';
+}
+
+class Test extends Secure
+{
+    public function print()
+    {
+        echo $this->secrete;
+    }
+}
+
+$class = new Test();
+echo $class->print();
+
+?>
